d93acbfde6213d7cc5ab4486f3e05821d137359f
smartdevops <rajivece@outlook.com>
2018-01-12 11:22:27 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -1,4 +1,4 @@
-curl -s -u admin:admin -X GET http://10.0.0.94:9000/api/qualitygates/project_status?projectKey=com.github.bharath008:StudentEnrollmentWithSpring -o gate.json
+curl -s -u admin:admin -X GET http://10.0.0.94:9000/api/qualitygates/project_status?projectKey=com.github.elizabetht:StudentEnrollmentWithSpring -o gate.json
 gate=`grep -oP '\"status\"\:[^,]+' gate.json | head -1`
 #echo "Quality Gate " $gate
 if [[ $gate = *OK* ]] 
